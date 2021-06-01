0df4e750ed0b9debfa62e1bd33e73bee56988448
Kurt Hakim <kurthakim@gmail.com>
2021-06-02 00:57:57 +0700

    Change data from local array to 'https://jsonplaceholder.typicode.com/users'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import CardList from './CardList';
-import { robots } from './robots';
 import SearchBox from './SearchBox';
 import './App.css';
 
@@ -9,9 +8,15 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots,
+            robots: [],
             searchfield:''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then( response => response.json() )
+        .then( users => this.setState({robots: users}));
     }
 
     onSearchChange = (e) => {
@@ -22,15 +27,18 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter( robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         });
+        if(this.state.robots.length === 0) {
+            return <h1>Loading...</h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <CardList robots={filteredRobots}/>
+                </div>
+            );
 
-        return (
-            <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots}/>
-
-            </div>
-        );
+        }
     }
 }
 
