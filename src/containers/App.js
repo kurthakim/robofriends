48692deaef11d43a0af303271fec95bb87999b44
Kurt Hakim <kurthakim@gmail.com>
2021-06-03 23:25:44 +0700

    Revert "Changing Classes To Hooks"
    This reverts commit d4a20035bdcb700821b123094558e3d492cecb93.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,43 +1,48 @@
-import React, { useState, useEffect } from 'react'
+import React, { Component } from 'react'
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import ErrorBoundary from '../components/ErrorBoundary'
 import './App.css';
 import Scroll from '../components/Scroll'
 
-function App() {
-
-    const [robots, setRobots] = useState([]);
-    const [searchfield, setSearchfield] = useState('');
-    const [count, setCount] = useState(0);
-
-    useEffect( () => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then( response => response.json() )
-            .then( users => setRobots(users) );      
-    }, []);
-
-    const onSearchChange = (event) => {
-        setSearchfield(event.target.value)
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: [],
+            searchfield:''
+        }
     }
 
-    const filteredRobots = robots.filter( robot => {
-        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-    });
-    return !robots.length ?
-        <h1>Loading...</h1> :
-        (
-            <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={onSearchChange}/>
-                <Scroll>
-                    <ErrorBoundary>
-                        <CardList robots={filteredRobots}/>
-                    </ErrorBoundary>
-                </Scroll>
-            </div>
-        );
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then( response => response.json() )
+        .then( users => this.setState({robots: users}));
+    }
 
+    onSearchChange = (e) => {
+        this.setState({ searchfield: e.target.value})
+    }
+
+    render() {
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter( robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+        });
+        return !robots.length ?
+            <h1>Loading...</h1> :
+            (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <Scroll>
+                        <ErrorBoundary>
+                            <CardList robots={filteredRobots}/>
+                        </ErrorBoundary>
+                    </Scroll>
+                </div>
+            );
+    }
 }
 
 export default App;
